1fbc318570fa7492471f2c310f9c056e8639814d
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-12-09 00:22:04 +0330

    insert Services

---
+++ Services/Repository/ISelectedRoleManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fri2Ends.Identity.Services.Repository
+{
+    public interface ISelectedRoleManager
+    {
+    }
+}
---
+++ Services/Srevices/SelectedRoleManager.cs
@@ -0,0 +1,12 @@
+﻿using Fri2Ends.Identity.Services.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fri2Ends.Identity.Services.Srevices
+{
+    public class SelectedRoleManager : ISelectedRoleManager
+    {
+    }
+}
