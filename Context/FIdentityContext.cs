9499c68a26286c52c2f1e43c0e92792deff052de
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-12-09 00:23:37 +0330

    insert context

---
+++ Context/FIdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fri2Ends.Identity.Context
+{
+    public class FIdentityContext : DbContext
+    {
+        public FIdentityContext(DbContextOptions<FIdentityContext> options) : base(options)
+        {
+
+        }
+    }
+}
